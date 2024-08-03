02e533e7c29f9adf7c912ac9a87b7d5ee199270c
Solange Duhimbaze Ihirwe <159579750+solangeihirwe03@users.noreply.github.com>
2024-08-03 17:25:41 +0200

    finished landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import MainRoute from "./components/layout/MainRoute"
 import Home from "./pages/Home"
+import About from "./pages/About"
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainRoute />}>
           <Route index element={<Home />} />
+          <Route path="/about" element={<About/>}/>
         </Route>
       </Routes>
     </Router>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,23 +1,48 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import logo from "../../../public/assets/Hotelstar.svg"
 import Menubar from "./Menubar"
 import { IoMenu } from "react-icons/io5"
+import { NavLink } from "react-router-dom"
+import 'tailwindcss/tailwind.css';
 
 const Header = () => {
     const [isActive, setIsActive] = useState(false)
+    const [isScrolled, setIsScrolled] = useState(false)
+
+    const handleScroll = () => {
+        const offset = window.scrollY;
+
+        if (offset > 300) {
+            setIsScrolled(true)
+        } else {
+            setIsScrolled(false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
 
     const handleToogle = () => {
         setIsActive(!isActive)
     }
+
     return (
-        <div className="bg-none text-white flex justify-between px-6 w-full items-start md:items-center fixed z-50">
+        <div className={`text-white flex justify-between px-6 w-full items-start md:items-center fixed z-50 transition-colors duration-300 ${isScrolled ? "bg-black" : "bg-none"}`}>
             <div className="flex gap-2 md:text-3xl items-center">
                 <img src={logo} alt="Logo" className="w-16 py-2" />
                 <p className="font-semibold hidden md:block">Booking
                     <span className="text-[#28A745] uppercase">Bridge</span>
                 </p>
             </div>
-            <button className="float-right py-4 bg-black p-3 absolute right-5 md:hidden" onClick={handleToogle}>
+            <button
+                className="float-right py-4 bg-black p-3 absolute right-5 md:hidden"
+                onClick={handleToogle}
+            >
                 <IoMenu fontSize={35} />
             </button>
             {isActive && (
@@ -26,8 +51,29 @@ const Header = () => {
                 </div>
             )}
             <ul className="hidden md:flex items-center gap-6 text-lg font-medium">
-                <li>HOME</li>
-                <li>ABOUT US</li>
+                <li>
+                    <NavLink to={"/"}
+                        className={({ isActive }) =>
+                            isActive
+                                ? 'border-[#28A745] px-2 py-2 w-28 border-b-4'
+                                : 'text-white hover:border-[#28A745] px-2 py-2 hover:border-b-4 w-28 transition-colors duration-300'
+                        }>
+                        HOME
+                    </NavLink>
+                </li>
+
+                <li>
+                    <NavLink to={"/about"}
+                        className={({ isActive }) =>
+                            isActive
+                                ? 'border-[#28A745] px-2 py-2 w-24 border-b-4'
+                                : 'text-white hover:border-[#28A745] px-2 py-2 hover:border-b-4 w-24 transition-colors duration-300'
+                        }>
+                        ABOUT US
+                    </NavLink>
+
+                </li>
+
                 <li>HOTELS</li>
                 <li>ROOM</li>
                 <li>ROOMS</li>

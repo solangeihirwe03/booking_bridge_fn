d072ec484623def4684f5008f34afb49778412b8
Solange Duhimbaze Ihirwe <159579750+solangeihirwe03@users.noreply.github.com>
2024-09-13 21:45:50 +0200

    finished hotel details

--- src/pages/Hotel.tsx
+++ src/pages/Hotel.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import { useState} from "react";
+import { useState } from "react";
 import { IoCaretDown } from "react-icons/io5";
 import { useLocation, useParams } from "react-router-dom";
 import hotelItems from "../components/hotels/hotel";
@@ -7,27 +7,73 @@ import frame from "../../public/assets/Frame.svg";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css"
 
+const DropDown = ({
+  bookRoom,
+  handleRoomIncrement,
+  handleRoomDecrement
+}:{
+  bookRoom: number,
+  handleRoomIncrement: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  handleRoomDecrement: (e: React.MouseEvent<HTMLButtonElement>) => void;
+
+}) => {
+  
+  return (
+    <>
+      <div className="bg-[#fafafa] shadow-md absolute top-10 w-52 h-20 flex justify-center items-center gap-3">
+        <button 
+        className="w-10 h-10 rounded-full shadow-lg text-3xl"
+        onClick={handleRoomDecrement}
+        >-</button>
+        <p>{bookRoom}</p>
+        <button
+          className="w-10 h-10 rounded-full shadow-lg text-3xl"
+          onClick={handleRoomIncrement}
+        >+</button>
+      </div>
+    </>
+  )
+}
 
 const Hotel = () => {
   const { id } = useParams()
   const [selectedCheckInDate, setSelectedCheckInDate] = useState<Date | null>(new Date())
   const [selectedCheckOutDate, setSelectedCheckOutDate] = useState<Date | null>(new Date())
+  const [isDropdownOpen, setIsDropdownOpen] = useState(true)
 
- const today = new Date()
-
- const handleCheckInChange = (date: Date | null)=>{
-  setSelectedCheckInDate(date)
-  if(date){
-    const nextDay = new Date(date);
-    nextDay.setDate(nextDay.getDate() + 1)
-
-    setSelectedCheckOutDate(nextDay)
+  const handleDropDown = () => {
+    setIsDropdownOpen(!isDropdownOpen)
   }
- }
 
- const handleCheckOutChange = (date: Date | null)=>{
-  setSelectedCheckOutDate(date)
- }
+  const [bookRoom, setBookRoom] = useState(1)
+  
+  const handleRoomIncrement = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    setBookRoom(prevCount => prevCount + 1)
+  }
+
+  const handleDecrement = (e:React.MouseEvent<HTMLButtonElement>)=>{
+    e.preventDefault()
+    if(bookRoom >= 1){
+      setBookRoom(prevCount=> prevCount - 1)
+    }
+  }
+
+  const today = new Date()
+
+  const handleCheckInChange = (date: Date | null) => {
+    setSelectedCheckInDate(date)
+    if (date) {
+      const nextDay = new Date(date);
+      nextDay.setDate(nextDay.getDate() + 1)
+
+      setSelectedCheckOutDate(nextDay)
+    }
+  }
+
+  const handleCheckOutChange = (date: Date | null) => {
+    setSelectedCheckOutDate(date)
+  }
 
   if (!id) {
     return <div>Hotel ID not found</div>;
@@ -38,7 +84,7 @@ const Hotel = () => {
   return (
 
     <>
-      <div className="bg-hotel h-screen  bg-center w-full bg-cover flex items-center justify-center ">
+      <div className="bg-hotel h-[100vh]  bg-center w-full  bg-cover flex items-center justify-center ">
         <div className="absolute inset-0 bg-opacity-40 bg-overlay"></div>
         <div className="absolute inset-0 flex items-center justify-center">
           <h1 className="text-white text-8xl font-semibold font-nautigal">{hotel.HotelName}</h1>
@@ -49,14 +95,14 @@ const Hotel = () => {
         <div className="border-r-2 px-6 border-[#6E7A73]">
           <p className="text-3xl text-[#6E7A73]">Check In</p>
           <div className="flex gap-2 items-center" >
-              <DatePicker
+            <DatePicker
               selected={selectedCheckInDate}
               onChange={handleCheckInChange}
               dateFormat="MMMM dd, yyyy"
               minDate={today}
               className="text-black"
             />
-            
+
           </div>
         </div>
 
@@ -79,11 +125,20 @@ const Hotel = () => {
         </div>
         <div className="border-r-2 px-10 border-[#6E7A73]">
           <p className="text-3xl text-[#6E7A73]">Room</p>
-          <div className="flex gap-2 items-center">
-            <p>August 12, 2024</p>
-            <p>
-              <IoCaretDown />
-            </p>
+          <div className="flex gap-2 items-center relative justify-between">
+            <p>{bookRoom}</p>
+            <div>
+              <p onClick={handleDropDown}>
+                <IoCaretDown />
+              </p>
+              {isDropdownOpen && (
+                <DropDown 
+                bookRoom={bookRoom}
+                handleRoomDecrement={handleDecrement}
+                handleRoomIncrement={handleRoomIncrement}
+                />
+              )}
+            </div>
           </div>
         </div>
         <div className="border-r-2 px-10 border-[#6E7A73]">
@@ -101,9 +156,9 @@ const Hotel = () => {
         <h1 className="text-[#1E1E1E] text-8xl font-semibold font-nautigal">{hotel.HotelName}</h1>
         <div className="flex gap-20 items-center justify-center">
           <div className="w-1/3">
-            <img 
-            src={hotel.hotelImage}
-            className="w-full"
+            <img
+              src={hotel.hotelImage}
+              className="w-full"
             />
           </div>
           <div className="w-2/5">
